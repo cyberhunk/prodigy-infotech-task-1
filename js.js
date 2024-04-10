c90fb75320c032c1ce968a8a2b8b8dfede99e151
cyberhunk <satya.pk0011@gmail.com>
2024-04-10 22:53:43 +0530

    frist commit

---
+++ js.js
@@ -0,0 +1,10 @@
+var list = document.querySelector(".list");
+var ham = document.querySelector(".ham");
+var img = document.querySelector("img")
+
+ham.addEventListener("click", function() {
+    list.style.opacity = "10";
+    img.style.opacity = "0.1"
+
+});
+
